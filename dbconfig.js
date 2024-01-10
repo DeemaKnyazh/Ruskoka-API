64677fc1f4d9a284e0881118cad69a14b3e84167
DeemaKnyazh <deemakn@yahoo.ca>
2024-01-09 21:46:03 -0500

    port fix

--- dbconfig.js
+++ dbconfig.js
@@ -4,7 +4,7 @@ const config = {
     user: process.env.AZURE_SQL_USERNAME || process.env.USER,
     password: process.env.PASSWORD || process.env.AZURE_SQL_PASSWORD,
     server: process.env.SERVER || process.env.AZURE_SQL_SERVER,
-    port: parseInt(process.env.PORT, 10) || parseInt(process.env.AZURE_SQL_PORT, 10),
+    port: 1433,
     database: process.env.DATABASE || process.env.AZURE_SQL_DATABASE,
     options:{
         trustedconnection: true,
